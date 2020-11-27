3d5dae90b0ab766f779f20879c161605a46997e9
Hsueh <1150932336@qq.com>
2020-11-27 18:57:52 +0800

    修改了代码

--- src/main/java/com/atguigu/springboot/love.java
+++ src/main/java/com/atguigu/springboot/love.java
@@ -4,5 +4,6 @@ public class love {
     public static void main(String[] args) {
         System.out.println("帅哥好!!!  plmm");
         System.out.println("帅哥好!!!  dxmm");
+        System.out.println("美女好!!!");
     }
 }

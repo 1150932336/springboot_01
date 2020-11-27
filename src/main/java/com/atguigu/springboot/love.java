f33a88e7078e53ff364ff0df5d7916c798c480cb
Hsueh <1150932336@qq.com>
2020-11-27 18:20:47 +0800

    plmm

--- src/main/java/com/atguigu/springboot/love.java
+++ src/main/java/com/atguigu/springboot/love.java
@@ -2,6 +2,6 @@ package com.atguigu.springboot;
 
 public class love {
     public static void main(String[] args) {
-        System.out.println("帅哥好!!!");
+        System.out.println("帅哥好!!!  plmm");
     }
 }
